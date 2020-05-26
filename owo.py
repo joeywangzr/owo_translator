41f7a8581a97f2eb5290e239fc5c6e6681d92ffd
joeywangzr <joeywangzr@gmail.com>
2020-05-26 11:27:00 -0600

    i'm sorry
    i shall atone for my sins

---
+++ owo.py
@@ -0,0 +1,12 @@
+def owo(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "rl":
+            translation = translation + "w"
+        elif letter in "RL":
+            translation = translation + "W"
+        else:
+            translation = translation + letter
+    return translation 
+
+print(owo(input("Input a phrase: ")))
